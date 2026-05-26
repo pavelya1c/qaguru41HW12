d2d10c927b4ede794bf5dc8f3e93a58cc2aabfff
Pavel Ya <pavelya1c@gmail.com>
2026-05-26 12:11:19 +0400

    getPropertyAdd

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -11,12 +11,14 @@ import static com.codeborne.selenide.Selenide.closeWebDriver;
 public class TestBase {
     @BeforeAll
     static void beforeAll() {
-        Configuration.baseUrl = "https://demoqa.com";
-        Configuration.browserSize = "1920x1080";
-//        Configuration.pageLoadStrategy = "eager";
-//        Configuration.timeout = 5000;
-//        Configuration.headless = false;
-     //dle    Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        Configuration.baseUrl = System.getProperty("baseUrl", "https://demoqa.com");
+        Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
+        Configuration.browser = System.getProperty("browser", "chrome");
+        Configuration.pageLoadStrategy = System.getProperty("pageLoadStrategy", "eager");
+        Configuration.remote = System.getProperty("remote");
+        Configuration.headless = Boolean.parseBoolean(System.getProperty("headless", "false"));
+        Configuration.holdBrowserOpen = Boolean.parseBoolean(System.getProperty("holdBrowserOpen", "false"));
+   //     Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options", Map.<String, Object>of(
                 "enableVNC", true,
